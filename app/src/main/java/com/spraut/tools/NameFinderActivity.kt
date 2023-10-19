c1b6b2fe9c29dadd1686131b8ea7e166bdb82c62
SprAut <ff1493706520@163.com>
2023-10-19 20:58:04 +0800

    优化UI

--- app/src/main/java/com/spraut/tools/NameFinderActivity.kt
+++ app/src/main/java/com/spraut/tools/NameFinderActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,8 +21,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
@@ -51,31 +59,46 @@ fun NameMatchTools() {
     val absentList = remember {
         mutableStateOf(listOf<String>())
     }
-    Column {
-        TextField(
-            value = fullNames.value,
-            onValueChange = { fullNames.value = it },
-            label = { Text(text = "全体名单") },
-            modifier = Modifier.padding(16.dp)
-        )
+    Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        TextField(
-            value = currentNames.value,
-            onValueChange = { currentNames.value = it },
-            label = { Text(text = "当次名单") },
-            modifier = Modifier.padding(16.dp)
-        )
+        NameInputBox(names = fullNames, label = "全班名单")
+        Spacer(modifier = Modifier.height(10.dp))
+
+        NameInputBox(names = currentNames, label = "本次名单")
+        Spacer(modifier = Modifier.height(10.dp))
 
         Button(onClick = {
             val fullList = separateNames(fullNames.value.text)
             val currentList = separateNames(currentNames.value.text)
             absentList.value = currentList.findWhichAreNotExistIn(fullList)
         }) {
-            Text(text = "查找缺失的名字")
+            Text(text = "查找缺失的同学")
         }
 
-        Text(text = "以下名字缺失：\n${absentList.value}")
+        Text(text = "缺少以下同学：\n${absentList.value}")
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NameInputBox(
+    names: MutableState<TextFieldValue>,
+    label: String = "",
+    modifier: Modifier = Modifier.fillMaxWidth()
+) {
+    TextField(
+        value = names.value,
+        onValueChange = { names.value = it },
+        label = { Text(text = label) },
+        modifier = modifier,
+        maxLines = 8
+    )
 }
 
 fun separateNames(names: String): List<String> {
